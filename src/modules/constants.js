58ad97d9e42056d2f46735ae664aaf9b1d18987e
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-06-05 16:32:07 +0530

    Implement direction constant and coordinate class

---
+++ src/modules/constants.js
@@ -0,0 +1,1 @@
+export const directions = { horizontal: 0, vertical: 1 };
---
+++ src/modules/coordinate.js
@@ -0,0 +1,23 @@
+export class Coordinate2D {
+  #row;
+  #column;
+
+  constructor(row, column) {
+    if (!this.#isValid(row) || !this.#isValid(column))
+      throw new Error('Invalid Coordinate');
+    this.#row = row;
+    this.#column = column;
+  }
+
+  get row() {
+    return this.#row;
+  }
+
+  get column() {
+    return this.#column;
+  }
+
+  #isValid(coord) {
+    return typeof coord === 'number' && Number.isInteger(coord) && coord >= 0;
+  }
+}
