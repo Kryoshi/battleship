85edf7caba9459354b970fc45dce25e126d94fab
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-06-05 14:53:27 +0530

    Modify ship tests

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -4,12 +4,12 @@ import { Ship } from '../modules/ship';
 describe('Ship object', () => {
   it('has length', () => {
     const length = 5;
-    expect(new Ship(length)).toHaveProperty('length', 5);
+    expect(new Ship(length)).toHaveProperty('length', length);
   });
 
   it('has hitpoints', () => {
     const length = 5;
-    expect(new Ship(length)).toHaveProperty('hitPoints', 5);
+    expect(new Ship(length)).toHaveProperty('hitPoints', length);
   });
 
   it('can be hit', () => {
@@ -17,9 +17,9 @@ describe('Ship object', () => {
     const ship = new Ship(length);
     expect(ship.hit).toBeDefined();
     ship.hit();
-    expect(ship).toHaveProperty('hitPoints', 4);
+    expect(ship).toHaveProperty('hitPoints', length - 1);
     ship.hit();
-    expect(ship).toHaveProperty('hitPoints', 3);
+    expect(ship).toHaveProperty('hitPoints', length - 2);
   });
 
   it('cannot have negative hitpoints', () => {
@@ -33,9 +33,12 @@ describe('Ship object', () => {
     const l = 2;
     const ship = new Ship(l);
     expect(ship).toHaveProperty('isSunk', false);
+
+    //When hit for max hitpoints
     for (let i = 0; i < l; ++i) ship.hit();
     expect(ship).toHaveProperty('isSunk', true);
 
+    //When hit beyond max hitpoints
     ship.hit();
     expect(ship).toHaveProperty('isSunk', true);
   });
